04c701f5da1f1110572bd38930e5837d7bceb2ea
zhangqi234110 <51742008+zhangqi234110@users.noreply.github.com>
2020-09-06 08:19:31 +0800

    Update Heq.java

--- src/main/java/com/example/huanyuan/Heq.java
+++ src/main/java/com/example/huanyuan/Heq.java
@@ -2,7 +2,8 @@ package com.example.huanyuan;
 
 public class Heq {
     public static void main(String[] args) {
-        System.out.println("第一版本");
+        System.out.println("第二版本");
         System.out.println("nihao");
+        System.out.println("恶趣味");
     }
 }
