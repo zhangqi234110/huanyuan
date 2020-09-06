cd64d81ba219de591c9e6f597ac3efb5d1de2d4a
zhangqi234110 <314890945@qq.com>
2020-09-06 08:20:20 +0800

    change

--- src/main/java/com/example/huanyuan/Heq.java
+++ src/main/java/com/example/huanyuan/Heq.java
@@ -4,5 +4,6 @@ public class Heq {
     public static void main(String[] args) {
         System.out.println("第一版本");
         System.out.println("nihao");
+        System.out.println("asda");
     }
 }
